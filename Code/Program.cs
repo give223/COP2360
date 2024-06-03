63a28b77c116b9bad31d9bef521c0536343f510b
give223 <117595480+give223@users.noreply.github.com>
2024-06-02 21:40:43 -0400

    Add files via upload

---
+++ Code/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+public class Panda
+{
+  string name;     
+  int age;            
+  public Panda (string n)
+  {
+    name = n; 
+  }
+  public Panda (string n, int a)
+  {
+    name = n; 
+    age = a;
+  }
+  public void Print()
+  {
+    if (age == 0)
+    {
+        Console.WriteLine("Panda" + name + " has no recorded age! Would you like to add one?");
+        string answer;
+        answer = Console.ReadLine();
+        answer = answer.ToUpper();
+        if (answer == "YES")
+        {
+            Console.WriteLine("Great! What is it?");
+            string inputAge;
+            inputAge = Console.ReadLine();
+            if (Int32.TryParse(inputAge, out age))
+            {
+                Print();
+            }
+            else
+            {
+                Console.WriteLine("That is not a number!");
+            }
+        }
+        else
+        {
+          
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Panda " + name + " is " + age + " years old.");
+  }
+    }
+}
+
+
